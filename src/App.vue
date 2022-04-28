93604d720414ee4dab69f79f1ae35d3945f5a8e1
Omar Terrazas <terrazas.omar@gmail.com>
2022-04-28 14:49:56 -0500

    Teleports

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,17 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <button @click="show = !show">Menu</button>
-  <transition name="fade">
-    <MenuItem v-show="show" />
-  </transition>
+  <div>
+    <ModalItem />
+  </div>
 </template>
 
 <script>
-import MenuItem from "./components/MenuItem.vue";
+import ModalItem from "./components/ModalItem.vue";
 
 export default {
   name: "App",
   components: {
-    MenuItem,
-  },
-  data() {
-    return {
-      show: false,
-    };
+    ModalItem,
   },
 };
 </script>
@@ -30,16 +24,5 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
-
-.fade-leave-to,
-.fade-enter-from {
-  opacity: 0;
-}
-
-.fade-enter-active,
-.fade-leave-active {
-  border: 1px solid #41b883;
-  transition: opacity 0.5s ease;
 }
 </style>
---
+++ src/components/ModalItem.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <button @click="toggle">Modal</button>
+    <teleport to="body">
+      <div v-show="show" class="modal">
+        <h1>Title</h1>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Non ratione
+          ex dolores ut quod maxime dolor quos consequatur, perferendis, harum
+          nesciunt quas neque molestiae magnam repellendus similique animi
+          praesentium nihil?
+        </p>
+        <button @click="toggle">Close</button>
+      </div>
+    </teleport>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ModalItem",
+  data() {
+    return {
+      show: false,
+    };
+  },
+  methods: {
+    toggle() {
+      this.show = !this.show;
+    },
+  },
+};
+</script>
